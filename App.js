1d57ef8430fe3fd640401f209ce1687708ec23bc
Shantela21 <shantelaslie@gmail.com>
2025-08-15 12:31:40 +0200

    modify App.js

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
-let sentence = "Hello World, I am Shantela";
+// let sentence = "Hello World, I am Shantela";
 
 
-// console.log(sentence[3]);
+// // console.log(sentence[3]);
 
-for (let i = 0; i<6; i++){
-// console.log(sentence[i]);
-} 
-for (x of sentence){
-    // console.log(x)
-}
-while (i<6){
-   console.log(sentence[i]); 
-}
+// for (let i = 0; i<6; i++){
+// // console.log(sentence[i]);
+// } 
+// for (x of sentence){
+//     // console.log(x)
+// }
+// // while (i<6){
+//    console.log(sentence[i]); 
+// }
 
 
 // let listBook = ["titanic", "extraction", "car"]
-// let i = 0;
+
 // for (let i = 0; i<6; i++){
 // console.log(listBook[i] [0]);
 // }
 // for (x of listBook){
-//     console.log(x)
+//     console.log(i)
 // }
-
+// let i = 0;
+// while (i<6){
+//    console.log(sentence[i]); 
+//    [E3]
+// }
 // let marks = {
 //     johndoe : 85,
 //     tom : 30,
@@ -32,3 +36,17 @@ while (i<6){
 // for (key in object){
 //     console.log(marks[Tom]);
 // }
+
+let condition = true;
+do {
+    console.log("Do While is running");
+   condition = false;
+}
+while(condition)
+
+    let j = 10; 
+do {
+    console.log("Another Do While is running");
+   j++;
+}
+while(j < 6)
